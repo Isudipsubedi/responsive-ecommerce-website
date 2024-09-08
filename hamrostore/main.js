46de634b4313c4e98605e69e6b3ed74adf9bfd11
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2024-09-08 22:25:05 +0545

    Displaying Dynamic Product Carts using JS

--- hamrostore/main.js
+++ hamrostore/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+import products from "./api/products.json";
+import { showProductContainer } from './homeProductCards';
+
+
+//to test whether the api is being fetched using console
+// console.log(products);
+
+// call the function to display all the products as a card
+
+//Define a function named "showProductContainer" that takes an array of products as input.
+showProductContainer(products);
