0caf1f067401836c30ca5e0043fe20c576a51868
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2024-09-09 18:08:54 +0545

    retrieving cart products from local storage

--- hamrostore/addToCart.js
+++ hamrostore/addToCart.js
@@ -1,11 +1,16 @@
+import { getCartProductFromLS } from "./getCartProducts";
+
 export const addToCart = (event, id, stock)=>{
+
+    let arrLocalStorageProduct = getCartProductFromLS();
+
     const currentProdElement = document.querySelector(`#card${id}`);
   //  console.log(currentProdElement);
 
     let quantity = currentProdElement.querySelector('.productQuantity').innerText;
     let price = currentProdElement.querySelector('.productPrice').innerText;
 
-  //  console.log(quantity, price);
+  // console.log(quantity, price);
 
-  
+
 };
