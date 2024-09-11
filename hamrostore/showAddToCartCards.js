f6da44b6a2eb81dfce51bae65c5736dd24b65571
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2024-09-11 18:30:36 +0545

    Display add to cart products & retrieve org pric & data

--- hamrostore/showAddToCartCards.js
+++ hamrostore/showAddToCartCards.js
@@ -1,4 +1,5 @@
 import products from "./api/products.json";
+import { fetchQuantityFromCartLS } from "./fetchQuantityFromCartLS";
 import { getCartProductFromLS } from "./getCartProducts";
 
 let cartProducts = getCartProductFromLS();
@@ -11,3 +12,31 @@ let filterProducts = products.filter((curProd)=>{
 
 console.log(filterProducts);
 
+//TO UPDATGE THE addToCart page
+
+const cartElement = document.querySelector("#productCartContainer");
+const templateContainer = document.querySelector("#productCartTemplate");
+ 
+
+const showCartProduct = ()=>{
+  filterProducts.forEach((curProd) => {
+    const {category, id, image, name, stock, price} = curProd;
+
+    let productClone = document.importNode(templateContainer.content, true);
+
+    const lSActualData = fetchQuantityFromCartLS(id, price);
+
+    productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
+    productClone.querySelector(".category").textContent = category;
+    productClone.querySelector(".productName").textContent = name;
+    productClone.querySelector(".productImage").src = image;
+
+    productClone.querySelector(".productQuantity").textContent = lSActualData.quantity;
+    productClone.querySelector(".productPrice").textContent =lSActualData.price;
+
+    cartElement.appendChild(productClone);
+  });
+};
+//Showing Cart Products
+
+showCartProduct();
